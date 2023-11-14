69d0813ffdacd9ab422317b1afae7d1862fac255
david001 <o.akintayo@alustudent.com>
2023-11-14 00:36:23 -0800

    update1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,6 +7,6 @@ class Square:
     def __init__(self, size):
         """Constructor.
         Args:
-        size: length of side of the square
+        size: length of side of the square.
         """
-        self __size = size
+        self.__size = size
